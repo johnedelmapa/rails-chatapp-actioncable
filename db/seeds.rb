55bd1661a6adb6a91b96462ab0e9df780834433a
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2021-04-26 21:59:49 +0800

    Create user and message resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Maps", password: "password")
+User.create(username: "John", password: "password")
+User.create(username: "Robert", password: "password")
+User.create(username: "Clint", password: "password")
+User.create(username: "James", password: "password")
+
+
+Message.create(body: "Hello Everyone", user_id: 1)
+Message.create(body: "Hey new guy", user_id: 2)
+Message.create(body: "Robert", user_id: 3)
+Message.create(body: "Im chillin", user_id: 4)
+Message.create(body: "I am new here", user_id: 5)
